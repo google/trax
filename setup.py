8e2c6eb11c78040c3fcf69eb00bc9568e1acaf9f
Trax Team <no-reply@google.com>
2019-10-07 21:55:40 -0700

    Project import generated by Copybara.
    FolderOrigin-RevId: /google/src/cloud/afrozm/trax-release-4/google3/..

---
+++ setup.py
@@ -0,0 +1,60 @@
+# Copyright 2019 The Trax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Install trax."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='trax',
+    version='1.0.0',
+    description='Trax',
+    author='Google Inc.',
+    author_email='no-reply@google.com',
+    url='http://github.com/google/trax',
+    license='Apache 2.0',
+    packages=find_packages(),
+    install_requires=[
+        'gin-config',
+        'gym',
+        'numpy',
+        'scipy',
+        'six',
+        'jax',
+        'jaxlib',
+        'tensorflow-datasets',
+        'absl-py',
+    ],
+    extras_require={
+        'tensorflow': ['tensorflow>=1.14.0'],
+        'tensorflow_gpu': ['tensorflow-gpu>=1.14.0'],
+        'tests': [
+            'attrs',
+            'pytest',
+            'mock',
+            'pylint',
+            'jupyter',
+            'matplotlib',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    keywords='tensorflow machine learning jax',
+)
---
+++ trax/layers/normalization.py
@@ -0,0 +1,137 @@
+# coding=utf-8
+# Copyright 2019 The Trax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Trax normalization layers."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from trax.backend import numpy as np
+from trax.layers import base
+
+
+class BatchNorm(base.Layer):
+  """Batch normalization."""
+
+  def __init__(self, axis=(0, 1, 2), epsilon=1e-5, center=True, scale=True,
+               momentum=0.999, mode='train'):
+    super(BatchNorm, self).__init__()
+    self._axis = axis
+    self._epsilon = epsilon
+    self._center = center
+    self._scale = scale
+    self._momentum = momentum
+    self._mode = mode
+
+  def new_params_and_state(self, input_shape, input_dtype, rng):
+    """Helper to initialize batch norm params."""
+    del input_dtype, rng
+    axis = self._axis
+    axis = (axis,) if np.isscalar(axis) else axis
+    shape = tuple(d for i, d in enumerate(input_shape) if i not in axis)
+    beta = np.zeros(shape, dtype='float32') if self._center else ()
+    gamma = np.ones(shape, dtype='float32') if self._scale else ()
+    def get_stats_axis(i, d):
+      if i in axis:
+        return 1
+      else:
+        return d
+    stats_shape = tuple(get_stats_axis(i, d) for i, d in enumerate(input_shape))
+    running_mean = np.zeros(stats_shape, dtype=np.float32)
+    running_var = np.ones(stats_shape, dtype=np.float32)
+    n_batches = np.zeros((), dtype=np.int32)
+    params = (beta, gamma)
+    state = (running_mean, running_var, n_batches)
+    return params, state
+
+  def _fast_mean_and_variance(self, x):
+    mean = np.mean(x, self._axis, keepdims=True)
+    # Fast but less numerically-stable variance calculation than np.var.
+    m1 = np.mean(x**2, self._axis, keepdims=True)
+    variance = m1 - mean**2
+    return mean, variance
+
+  def _exponential_smoothing(self, new, old):
+    smoothed_value = self._momentum * old + (1 - self._momentum) * new
+    return smoothed_value.astype(old.dtype)
+
+  def _z_score(self, x, mean, variance):
+    mu = mean.astype(x.dtype)
+    sigma = np.sqrt(variance + self._epsilon).astype(x.dtype)
+    return (x - mu) / sigma
+
+  def _beta_gamma_with_correct_axes(self, x, params):
+    # Expand the parameters to have the right axes.
+    beta, gamma = params
+    # TODO(phawkins): np.expand_dims should accept an axis tuple.
+    # (https://github.com/numpy/numpy/issues/12290)
+    ed = tuple(None if i in self._axis else slice(None)
+               for i in range(np.ndim(x)))
+    beta = beta[ed]
+    gamma = gamma[ed]
+    return beta, gamma
+
+  def forward(self, x, params, state, **unused_kwargs):
+    """Computes batch normalization as part of a forward pass in the model."""
+
+    running_mean, running_var, n_batches = state
+    if self._mode == 'train':
+      n_batches += 1
+      mean, var = self._fast_mean_and_variance(x)
+      running_mean = self._exponential_smoothing(mean, running_mean)
+      running_var = self._exponential_smoothing(var, running_var)
+      state = (running_mean, running_var, n_batches)
+    else:
+      mean = running_mean
+      var = running_var
+
+    z = self._z_score(x, mean, var)
+    beta, gamma = self._beta_gamma_with_correct_axes(x, params)
+
+    # Return the z rescaled by the parameters if requested.
+    if self._center and self._scale:
+      output = gamma * z + beta
+    elif self._center:
+      output = z + beta
+    elif self._scale:
+      output = gamma * z
+    else:
+      output = z
+    assert output.dtype == x.dtype, ('The dtype of the output (%s) of batch '
+                                     'norm is not the same as the input (%s). '
+                                     'Batch norm should not change the dtype' %
+                                     (output.dtype, x.dtype))
+    return output, state
+
+
+# Layer normalization.
+def _layer_norm_params_and_state(input_shape, input_dtype, rng):
+  """Helper: create layer norm parameters."""
+  del input_dtype, rng
+  features = input_shape[-1]
+  scale = np.ones(features)
+  bias = np.zeros(features)
+  params = (scale, bias)
+  return params, ()
+
+
+@base.layer(new_params_and_state_fn=_layer_norm_params_and_state)
+def LayerNorm(x, params, epsilon=1e-6, **unused_kwargs):  # pylint: disable=invalid-name
+  (scale, bias) = params
+  mean = np.mean(x, axis=-1, keepdims=True)
+  variance = np.mean((x - mean)**2, axis=-1, keepdims=True)
+  norm_inputs = (x - mean) / np.sqrt(variance + epsilon)
+  return norm_inputs * scale + bias
