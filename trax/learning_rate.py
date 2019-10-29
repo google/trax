149e346058936ccf156de812f907912feb3fc8d0
Afroz Mohiuddin <afrozm@google.com>
2019-10-29 13:42:31 -0700

    Correct too many values to unpack error.
    PiperOrigin-RevId: 277355309

--- trax/learning_rate.py
+++ trax/learning_rate.py
@@ -229,7 +229,7 @@ def PolicySchedule(
   # (opt_state, state, epoch, opt_step, history)
   (opt_state, state, _, _, _) = ppo.maybe_restore_opt_state(policy_dir)
   assert opt_state is not None, 'Policy checkpoint not found.'
-  (params, _) = opt_state
+  (params, _, _) = opt_state
   logging.vlog(
       1, 'Restoring the policy parameters took %0.2f sec.',
       time.time() - start_time
