4222b3f8f4b3715ba85b0f7e7e262d6cb1b2fa09
Afroz Mohiuddin <afrozm@google.com>
2019-11-01 16:40:22 -0700

    Trax v1.1.0
    PiperOrigin-RevId: 278024917

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name='trax',
-    version='1.0.1',
+    version='1.1.0',
     description='Trax',
     author='Google Inc.',
     author_email='no-reply@google.com',
