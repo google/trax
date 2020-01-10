14287335d7502084d816046f6068b4b083caec52
Jonni Kanerva <jonni@google.com>
2020-01-10 09:14:14 -0800

    Move Optimizer class to front of file and rename private functions.
    PiperOrigin-RevId: 289104624

--- trax/optimizers/base.py
+++ trax/optimizers/base.py
@@ -23,45 +23,6 @@ from trax.layers import base as layers
 from trax.math import numpy as np
 
 
-def tree_flatten(tree):
-  """Flatten a tree into a list."""
-  if isinstance(tree, (list, tuple)):
-    # In python, sum of lists starting from [] is the concatenation.
-    return sum([tree_flatten(t) for t in tree], [])
-  if isinstance(tree, dict):
-    # Only use the values in case of a dictionary node.
-    return sum([tree_flatten(v) for v in tree.values()], [])
-  return [tree]
-
-
-def tree_unflatten(flat, tree):
-  """Unflatten a list into a tree given the tree shape as second argument.
-
-  Args:
-    flat: a flat list of elements to be assembled into a tree.
-    tree: a tree with the structure we want to have in the new tree.
-
-  Returns:
-    A pair (new_tree, rest_of_flat) where the new tree that has the structure
-    of tree but with leaves from flat, and the remaining elements of flat if
-    more were provided than the number of leaves of tree (useful for recursion).
-  """
-  if isinstance(tree, (list, tuple)):
-    new_tree, rest = [], flat
-    for t in tree:
-      new_t, rest = tree_unflatten(rest, t)
-      new_tree.append(new_t)
-    new_tree = tuple(new_tree) if isinstance(tree, tuple) else new_tree
-    return new_tree, rest
-  if isinstance(tree, dict):
-    new_tree, rest = {}, flat
-    for k in tree:
-      new_v, rest = tree_unflatten(rest, tree[k])
-      new_tree[k] = new_v
-    return new_tree, rest
-  return flat[0], flat[1:]
-
-
 class Optimizer(object):
   """Optimizer object, base class. Maps per-parameter functions to trees."""
 
@@ -107,7 +68,7 @@ class Optimizer(object):
 
   def tree_init(self, param_tree):
     return (
-        [self.init(param) for param in tree_flatten(param_tree)],
+        [self.init(param) for param in _tree_flatten(param_tree)],
         self._init_opt_params,
     )
 
@@ -125,15 +86,54 @@ class Optimizer(object):
     return new_params, new_slots
 
   def tree_update(self, step, grad_tree, param_tree, slots, opt_params):
-    grads_flat = tree_flatten(grad_tree)
-    params_flat = tree_flatten(param_tree)
+    grads_flat = _tree_flatten(grad_tree)
+    params_flat = _tree_flatten(param_tree)
     updated_pairs = [
         self._update_and_check(step, grad, param, slot, opt_params)
         for (grad, param, slot) in zip(grads_flat, params_flat, slots)
     ]
     new_params_flat, new_slots = zip(*updated_pairs)
-    new_params, _ = tree_unflatten(new_params_flat, param_tree)
+    new_params, _ = _tree_unflatten(new_params_flat, param_tree)
     return new_params, new_slots
+
+
+def _tree_flatten(tree):
+  """Flatten a tree into a list."""
+  if isinstance(tree, (list, tuple)):
+    # In python, sum of lists starting from [] is the concatenation.
+    return sum([_tree_flatten(t) for t in tree], [])
+  if isinstance(tree, dict):
+    # Only use the values in case of a dictionary node.
+    return sum([_tree_flatten(v) for v in tree.values()], [])
+  return [tree]
+
+
+def _tree_unflatten(flat, tree):
+  """Unflatten a list into a tree given the tree shape as second argument.
+
+  Args:
+    flat: a flat list of elements to be assembled into a tree.
+    tree: a tree with the structure we want to have in the new tree.
+
+  Returns:
+    A pair (new_tree, rest_of_flat) where the new tree that has the structure
+    of tree but with leaves from flat, and the remaining elements of flat if
+    more were provided than the number of leaves of tree (useful for recursion).
+  """
+  if isinstance(tree, (list, tuple)):
+    new_tree, rest = [], flat
+    for t in tree:
+      new_t, rest = _tree_unflatten(rest, t)
+      new_tree.append(new_t)
+    new_tree = tuple(new_tree) if isinstance(tree, tuple) else new_tree
+    return new_tree, rest
+  if isinstance(tree, dict):
+    new_tree, rest = {}, flat
+    for k in tree:
+      new_v, rest = _tree_unflatten(rest, tree[k])
+      new_tree[k] = new_v
+    return new_tree, rest
+  return flat[0], flat[1:]
 
 
 # Utilities.
@@ -141,7 +141,7 @@ class Optimizer(object):
 
 def l2_norm(tree):
   """Compute the l2 norm of a pytree of arrays. Useful for weight decay."""
-  leaves = tree_flatten(tree)
+  leaves = _tree_flatten(tree)
   return np.sqrt(sum(np.vdot(x, x) for x in leaves))
 
 
