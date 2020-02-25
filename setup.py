6d31e7915f285bd88d51782d40b14987f6a7c2f2
Afroz Mohiuddin <afrozm@google.com>
2020-02-25 08:05:31 -0800

    Bump Trax version to 1.2.3
    PiperOrigin-RevId: 297118186

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 
 setup(
     name='trax',
-    version='1.2.2',
+    version='1.2.3',
     description='Trax',
     long_description=(
         'Trax helps you understand deep learning. We start with basic maths and'
