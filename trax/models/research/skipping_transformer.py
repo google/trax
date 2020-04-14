15b7faa82e47bd19178b314dfc83174ba2fd7b6c
Lukasz Kaiser <lukaszkaiser@google.com>
2020-04-14 16:46:26 -0700

    Corrections in skipping transformer and a step-based skipping schedule.
    PiperOrigin-RevId: 306540940

--- trax/models/research/skipping_transformer.py
+++ trax/models/research/skipping_transformer.py
@@ -32,6 +32,9 @@ class SkippingSerial(tl.Serial):
   def __init__(self, *sublayers, **kwargs):
     super(SkippingSerial, self).__init__(*sublayers)
     self._mode = kwargs.get('mode', 'train')
+    # Parameters for skipping: how many steps to warm-up, how often to skip.
+    self._skipping_warmup_steps = kwargs.get('skipping_warmup_steps', 20000)
+    self._skip_fraction = kwargs.get('skip_fraction', 0.4)
     # Ensure that each layer has the same number of inputs and outputs.
     if self.sublayers:
       n_in_out = self.sublayers[0].n_in
@@ -39,14 +42,33 @@ class SkippingSerial(tl.Serial):
         assert layer.n_in == n_in_out
         assert layer.n_out == n_in_out
 
+  def new_weights_and_state(self, input_signature):
+    """Add a step-counter to the state. Initialize with 0."""
+    weights, state = super(SkippingSerial, self).new_weights_and_state(
+        input_signature)
+    return weights, (0, state)
+
+  @tl.Layer.state.setter
+  def state(self, state):
+    """Recursively sets non-param state on this layer and all sublayers."""
+    self._state = state
+    n_layers = self._n_layers
+    if n_layers != 1 and len(state[1]) != n_layers:
+      raise ValueError(
+          f'Number of state elements ({len(state[1])}) does not equal '
+          f'number of sublayers ({n_layers}).')
+    for layer, sublayer_state in zip(self.sublayers, state[1]):
+      layer.state = sublayer_state
+
   def forward_with_state(self, xs, weights=tl.EMPTY_WEIGHTS,
                          state=tl.EMPTY_STATE, **kwargs):
     self._validate_forward_inputs(xs)
+    (step, layers_state) = state
     # Get N+1 rngs, N for running layers and one extra.
     rngs = _pop_rng_and_split(kwargs, self._n_layers + 1)
     rng0, rngs = rngs[0], rngs[1:]
     if not self.sublayers:  # No-op: leave args unchanged.
-      return (xs, state)
+      return (xs, (step + 1, layers_state))
 
     # Prepare the stack and do some safety checks as in the parent class.
     stack = xs
@@ -55,21 +77,33 @@ class SkippingSerial(tl.Serial):
     if n_layers != 1 and len(weights) != n_layers:
       raise ValueError('number of weights ({}) not equal to number of layers '
                        '({})'.format(len(weights), n_layers))
-    if n_layers != 1 and len(state) != n_layers:
+    if n_layers != 1 and len(layers_state) != n_layers:
       raise ValueError('length of state ({}) not equal to number of layers '
-                       '({})'.format(len(state), n_layers))
+                       '({})'.format(len(layers_state), n_layers))
 
     # TODO(chowdhery): try different strategies, also try running not all
     # layers backwards by using math.stop_gradient where needed.
 
     # Calculate how many layers to run forward.
     if self._mode == 'train':
-      n_forward_layers = random.uniform(rng0, (), np.float32, 0.0, n_layers)
+      # warmup goes from 1.0 at start to 0.0 at skipping_warmup_steps and after
+      w_steps = float(self._skipping_warmup_steps)
+      warmup = np.maximum(0.0, (w_steps - step.astype(np.float32)) / w_steps)
+      # low is the minimum number of layers to *not* skip, from n_layers to 0
+      low = warmup * float(n_layers)
+      # high should be so that (high - n_layers) / high = 1.0 - skip_fraction
+      # because (high - n_layers) / high is the probability we're not skipping
+      # (after warmup); so high - n_layers = high - high * skip_fraction
+      high = float(n_layers) / self._skip_fraction
+      # We want the same rng0 on all cores.
+      if math.device_count() > 1:
+        rng0 = math.psum(rng0, 'batch')
+      n_forward_layers = random.uniform(rng0, (), np.float32, low, high)
     else:
       n_forward_layers = float(n_layers)
     # Run layers skipping after a certain number.
     cur_layer_idx = 0.0
-    for layer, p, s, rng in zip(self.sublayers, weights, state, rngs):
+    for layer, p, s, rng in zip(self.sublayers, weights, layers_state, rngs):
       inputs = _inputs_from_stack(layer, stack)
       outputs, s = math.cond(  # Skip (do identity) if > n_forward_layers.
           pred=(math.lt(cur_layer_idx, n_forward_layers)),
@@ -81,7 +115,7 @@ class SkippingSerial(tl.Serial):
       stack = _outputs_onto_stack(layer, outputs, stack)
       new_state.append(s)
       cur_layer_idx += 1.0
-    return stack, new_state
+    return stack, (step + 1, new_state)
 
 
 def SkippingTransformerLM(vocab_size,
