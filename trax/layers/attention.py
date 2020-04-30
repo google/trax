483e0e5ff569bcd3ff25156b9a387412856370ef
Jonni Kanerva <jonni@google.com>
2020-04-29 17:37:15 -0700

    Code and docstring changes to show more of how attention works.
    Details:
      - Adapt docstrings to better fit our norms elsewhere in codebase.
      - Change a few variable names to work hand-in-hand with docstring
        explanations.
      - Remove asserts: either replace with if + ValueError or rearrange code
        so the asserts would no longer be load-bearing.
    PiperOrigin-RevId: 309132367

--- trax/layers/attention.py
+++ trax/layers/attention.py
@@ -32,38 +32,42 @@ from trax.math import numpy as jnp
 
 
 def Attention(d_feature, n_heads=1, dropout=0.0, mode='train'):
-  """Transformer-style multi-headed attention.
+  """Returns a layer that maps (activations, mask) to (new_activations, mask).
 
-  Accepts inputs of the form (x, mask) and constructs (q, k, v) from x.
+  This layer type represents one pass of multi-head self-attention, best
+  known for its central role in Transformer models. Internally, it:
+    - maps activations to `(queries, keys, values)` triples,
+    - splits `queries`, `keys`, and `values` into multiple 'heads',
+    - computes per-head attention weights from per-head `(queries, keys)`,
+    - applies `mask` to screen out positions that come from padding tokens,
+    - optionally applies dropout to attention weights,
+    - uses attention weights to combine per-head `values` vectors, and
+    - fuses per-head results into activations matching original input shapes.
 
   Args:
-    d_feature: int:  dimensionality of feature embedding
-    n_heads: int: number of attention heads
-    dropout: float: dropout rate
-    mode: str: 'train' or 'eval'
-
-  Returns:
-    Multi-headed self-attention result and the mask.
+    d_feature: Depth/dimensionality of feature embedding.
+    n_heads: Number of attention heads.
+    dropout: Probababilistic rate for internal dropout applied to attention
+        activations (based on query-key pairs) before dotting them with values.
+    mode: Either 'train' or 'eval'.
   """
   return cb.Serial(
-      cb.Dup(), cb.Dup(),
+      cb.Dup(), cb.Dup(),  # TODO(jonni): replace with Select([0, 0, 0])
       AttentionQKV(d_feature, n_heads=n_heads, dropout=dropout, mode=mode),
   )
 
 
 def AttentionQKV(d_feature, n_heads=1, dropout=0.0, mode='train'):
-  """Transformer-style multi-headed attention.
+  """Returns a layer that maps (q, k, v, mask) to (activations, mask).
 
-  Accepts inputs of the form q, k, v, mask.
+  See `Attention` above for further context/details.
 
   Args:
-    d_feature: int:  dimensionality of feature embedding
-    n_heads: int: number of attention heads
-    dropout: float: dropout rate
-    mode: str: 'train' or 'eval'
-
-  Returns:
-    Multi-headed self-attention result and the mask.
+    d_feature: Depth/dimensionality of feature embedding.
+    n_heads: Number of attention heads.
+    dropout: Probababilistic rate for internal dropout applied to attention
+        activations (based on query-key pairs) before dotting them with values.
+    mode: Either 'train' or 'eval'.
   """
   return cb.Serial(
       cb.Parallel(
@@ -78,7 +82,17 @@ def AttentionQKV(d_feature, n_heads=1, dropout=0.0, mode='train'):
 
 
 class PureAttention(base.Layer):
-  """Layer constructor function for a pure attention layer."""
+  """Layer that maps from (queries, keys, values, mask) to (activations, mask).
+
+  This layer type performs the inner workings of one pass of multi-head
+  self-attention. It:
+    - splits `queries`, `keys`, and `values` into multiple 'heads',
+    - computes per-head attention weights from per-head `(queries, keys)`,
+    - applies `mask` to screen out positions that come from padding tokens,
+    - optionally applies dropout to attention weights,
+    - uses attention weights to combine per-head `values` vectors, and
+    - merges per-head results into activations matching original input shapes.
+  """
 
   def __init__(self, n_heads=1, dropout=0.0, mode='train'):
     super(PureAttention, self).__init__(n_in=4, n_out=2)
@@ -86,69 +100,81 @@ class PureAttention(base.Layer):
     self._dropout = dropout
     self._mode = mode
 
-  def forward_with_state(self, x, weights, state, rng):
-    """Pure transformer-style multi-headed attention.
+  def forward_with_state(self, inputs, weights, state, rng):
+    """Returns attention-computed activations, unmodified mask, and state.
 
     Args:
-      x: inputs (q, k, v, mask)
-      weights: parameters (none)
-      state: parameters (none)
+      inputs: Tuple of (queries, keys, values, mask).
+      weights: Not used; should be empty tuple or list.
+      state: Not used; should be empty tuple or list.
       rng: Single-use random number generator (JAX PRNG key).
 
     Returns:
-      Pure Multi-headed attention result, and the mask.
+      Tuple of (outputs, state), where outputs is the pair (activations, mask).
     """
     del weights
-    n_heads, dropout, mode = self._n_heads, self._dropout, self._mode
-    q, k, v, mask = x
+    q, k, v, mask = inputs
+
     batch_size = q.shape[0]
     d_feature = q.shape[-1]
+    n_heads = self._n_heads
+    if d_feature % n_heads != 0:
+      raise ValueError(
+          f'Dimensionality of feature embedding ({d_feature}) is not a '
+          f'multiple of the requested number of attention heads ({n_heads}).')
 
-    assert d_feature % n_heads == 0
     d_head = d_feature // n_heads
 
     def _split_into_heads(x):
-      """Reshapes tensors to prepare for multi-headed computation."""
+      """Reshapes tensors to prepare for multi-head computation."""
       # (b_size, seq_len, d_feature) --> (b_size, n_heads, seq_len, d_head)
       x = x.reshape((batch_size, -1, n_heads, d_head))
       x = x.transpose((0, 2, 1, 3))
       return x
 
     def _merge_heads(x):
-      """Undoes splitting, post multi-headed computation."""
+      """Undoes splitting, after multi-head computation."""
       # (b_size, n_heads, seq_len, d_head) --> (b_size, seq_len, d_feature)
       x = x.transpose((0, 2, 1, 3))
       x = x.reshape((batch_size, -1, n_heads * d_head))
       return x
 
-    res = _merge_heads(
-        DotProductAttention(_split_into_heads(q),
-                            _split_into_heads(k),
-                            _split_into_heads(v),
-                            mask,
-                            dropout=dropout,
-                            mode=mode,
-                            rng=rng))
-    return (res, mask), state  # Keep the mask.
+    per_head_results = DotProductAttention(_split_into_heads(q),
+                                           _split_into_heads(k),
+                                           _split_into_heads(v),
+                                           mask,
+                                           dropout=self._dropout,
+                                           mode=self._mode,
+                                           rng=rng)
+    merged_results = _merge_heads(per_head_results)
+    return (merged_results, mask), state
 
 
-def DotProductAttention(query, key, value, mask, dropout, mode, rng):
-  """Core dot product self-attention.
+def DotProductAttention(queries, keys, values, mask, dropout, mode, rng):
+  """Computes new activations via masked attention-weighted sum of values.
+
+  This function is the core of the attention mechanism. It:
+    - computes per-head attention weights from per-head `(queries, keys)`,
+    - applies `mask` to screen out positions that come from padding tokens,
+    - optionally applies dropout to attention weights, and
+    - uses attention weights to combine per-head `values` vectors.
 
   Args:
-    query: array of representations
-    key: array of representations
-    value: array of representations
-    mask: attention-mask, gates attention
-    dropout: float: dropout rate
-    mode: 'eval' or 'train': whether to use dropout
+    queries: Per-head activations representing attention queries.
+    keys: Per-head activations representing attention keys.
+    values: Per-head activations to be combined by computed attention weights.
+    mask: Mask that distinguishes positions with real content vs. padding.
+    dropout: Probababilistic rate for dropout applied to attention activations
+        (based on query-key pairs) before dotting them with values.
+    mode: Either 'train' or eval'. Dropout applies only in 'train' mode.
     rng: Single-use random number generator (JAX PRNG key).
 
   Returns:
-    Self attention for q, k, v arrays.
+    Per-head activations resulting from masked per-head attention-weighted
+    sum of per-head values.
   """
-  depth = jnp.shape(query)[-1]
-  dots = jnp.matmul(query, jnp.swapaxes(key, -1, -2)) / jnp.sqrt(depth)
+  d_feature = queries.shape[-1]
+  dots = jnp.matmul(queries, jnp.swapaxes(keys, -1, -2)) / jnp.sqrt(d_feature)
   if mask is not None:
     # TODO(kitaev): workaround for https://github.com/google/jax/issues/850
     # We must ensure that both mask and the -1e9 constant have a data dependency
@@ -165,23 +191,28 @@ def DotProductAttention(query, key, value, mask, dropout, mode, rng):
   if dropout is not None and dropout > 0.0 and mode == 'train':
     keep = math.random.bernoulli(rng, 1.0 - dropout, dots.shape)
     dots = jnp.where(keep, dots / (1.0 - dropout), jnp.zeros_like(dots))
-  out = jnp.matmul(dots, value)
+  out = jnp.matmul(dots, values)
   return out
 
 
 def CausalAttention(d_feature, n_heads=1, dropout=0.0, mode='train'):
-  """Transformer-style multi-headed causal attention.
+  """Returns a layer that maps activations to activations, with causal masking.
+
+  Like `Attention`, this layer type represents one pass of multi-head
+  self-attention, but with causal masking in place of padding-based masking.
 
   Args:
-    d_feature: int:  dimensionality of feature embedding
-    n_heads: int: number of attention heads
-    dropout: float: attention dropout
-    mode: str: 'train' or 'eval'
-
-  Returns:
-    Multi-headed self-attention result.
+    d_feature: Depth/dimensionality of feature embedding.
+    n_heads: Number of attention heads.
+    dropout: Probababilistic rate for internal dropout applied to attention
+        activations (based on query-key pairs) before dotting them with values.
+    mode: Either 'train' or 'eval'.
   """
-  assert d_feature % n_heads == 0
+  if d_feature % n_heads != 0:
+    raise ValueError(
+        f'Dimensionality of feature embedding ({d_feature}) is not a multiple '
+        f'of the requested number of attention heads ({n_heads}).')
+
   d_head = d_feature // n_heads
 
   def _split_into_heads():
@@ -198,7 +229,7 @@ def CausalAttention(d_feature, n_heads=1, dropout=0.0, mode='train'):
     return Fn('SplitIntoHeads', f)
 
   def _merge_heads():
-    """Layer that undoes splitting, post multi-headed computation."""
+    """Layer that undoes splitting, after multi-head computation."""
     def f(x):
       seq_len = x.shape[1]
 
@@ -222,7 +253,7 @@ def CausalAttention(d_feature, n_heads=1, dropout=0.0, mode='train'):
 
 
 class DotProductCausalAttention(base.Layer):
-  """A standard (non-memory-efficient) dot product attention implementation."""
+  """Computes new activations via causally masked attention-weighted values."""
 
   def __init__(self, dropout=0.0, mode='train'):
     super(DotProductCausalAttention, self).__init__(n_in=3, n_out=1)
@@ -233,7 +264,11 @@ class DotProductCausalAttention(base.Layer):
                          state=base.EMPTY_STATE, rng=None):
     del weights
     q, k, v = inputs
-    if self._mode != 'predict':
+
+    if self._mode == 'predict':
+      state = _fast_inference_update_state(inputs, state)
+      (k, v, mask, _) = state
+    else:
       mask_size = q.shape[-2]
       # Not all backends define jnp.tril. However, using np.tril is inefficient
       # in that it creates a large global constant. TODO(kitaev): try to find an
@@ -244,25 +279,16 @@ class DotProductCausalAttention(base.Layer):
       else:
         mask = np.tril(
             np.ones((1, mask_size, mask_size), dtype=np.bool_), k=0)
-    else:
-      assert self._mode == 'predict'
-      state = _fast_inference_update_state(inputs, state)
-      (k, v, mask, _) = state
 
     res = DotProductAttention(
         q, k, v, mask, dropout=self._dropout, mode=self._mode, rng=rng)
     return res, state
 
   def new_weights_and_state(self, input_signature):
-    if self._mode != 'predict':
-      return base.EMPTY_WEIGHTS, base.EMPTY_STATE
-
-    assert self._mode == 'predict'
-    weights = base.EMPTY_WEIGHTS
-    # Buffer length is hardcoded for now. TODO(pkozakowski): Pass it from the
-    # model.
-    max_len = 2048
-    state = _fast_inference_init_state(input_signature, max_len)
+    weights, state = base.EMPTY_WEIGHTS, base.EMPTY_STATE
+    if self._mode == 'predict':
+      max_len = 2048  # Hardcoded.  TODO(pkozakowski): Pass it from the model.
+      state = _fast_inference_init_state(input_signature, max_len)
     return weights, state
 
 
@@ -286,8 +312,13 @@ def ShiftRight(n_shifts=1, mode='train'):
 
 
 def PaddingMask(pad=0):
+  """Layer to distinguish positions with real content/tokens vs. padding."""
   def f(x):
-    return jnp.reshape(x != pad, (x.shape[0], 1, 1, x.shape[-1]))
+    # TODO(jonni): Check/require that len(x.shape) == 2?
+    batch_size = x.shape[0]
+    d_feature = x.shape[-1]
+    content_positions = (x != pad)
+    return content_positions.reshape((batch_size, 1, 1, d_feature))
   return Fn(f'PaddingMask({pad})', f)
 
 
@@ -337,7 +368,10 @@ class PositionalEncoding(base.Layer):
         multiplier = keep.astype(x.dtype) / keep_prob
         return (x + px * multiplier, state)
     else:
-      assert self._dropout == 0
+      if self._dropout != 0:
+        raise ValueError(f'In predict mode, but dropout rate '
+                         f'({self._dropout}) is not zero.')
+
       # State in this class is only used for fast inference. In that case,
       # the model is called with consecutive elements position-by-position.
       # This positional encoding layer needs to store the index of the current
@@ -374,8 +408,8 @@ def _fast_inference_init_state(input_signature, buffer_length):
   """Returns an initial state for causal attention layer fast inference."""
   def zeros_for(batch_size, shape_dtype):
     shape, dtype = shape_dtype.as_tuple()
-    depth = shape[-1]
-    return jnp.zeros((batch_size, buffer_length, depth), dtype=dtype)
+    d_feature = shape[-1]
+    return jnp.zeros((batch_size, buffer_length, d_feature), dtype=dtype)
 
   batch_size = input_signature[0].shape[0]
   k = zeros_for(batch_size, input_signature[1])
@@ -387,23 +421,22 @@ def _fast_inference_init_state(input_signature, buffer_length):
 
 def _fast_inference_update_state(inputs, state):
   """Updates state of a causal attention layer for fast inference."""
-  assert math.backend_name() == 'jax', (
-      'JAX backend is required to use the predict mode.')
+  if math.backend_name() != 'jax':
+    raise ValueError(f'JAX backend is required in predict mode, but found '
+                     f'backend ({math.backend_nameO()}).')
   for x in inputs:
-    assert x.shape[1] == 1, (
-        'In predict mode the input sequence must be of length 1.')
+    if x.shape[1] != 1:
+      raise ValueError(f'In predict mode, input sequence must have length 1, '
+                       f'instead has length {x.shape[1]}.')
   # Fast inference: run with only 1 query in each step, storing the sequence
   # of keys and values calculated so far in state.
   (_, new_k, new_v) = inputs
   (ks, vs, mask, seq_indices) = state
   batch_indices = jnp.arange(ks.shape[0])
   ks = jax.ops.index_update(
-      ks, jax.ops.index[batch_indices, seq_indices, :], new_k[:, 0, :]
-  )
+      ks, jax.ops.index[batch_indices, seq_indices, :], new_k[:, 0, :])
   vs = jax.ops.index_update(
-      vs, jax.ops.index[batch_indices, seq_indices, :], new_v[:, 0, :]
-  )
+      vs, jax.ops.index[batch_indices, seq_indices, :], new_v[:, 0, :])
   mask = jax.ops.index_update(
-      mask, jax.ops.index[batch_indices, :, seq_indices], 1
-  )
+      mask, jax.ops.index[batch_indices, :, seq_indices], 1)
   return (ks, vs, mask, seq_indices + 1)
