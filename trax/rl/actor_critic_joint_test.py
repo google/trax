68bc18b2b84c4a7e3de70159357acaf5201f1007
Henryk Michalewski <henrykm@google.com>
2020-04-30 19:18:12 -0700

    Debugging prints
    PiperOrigin-RevId: 309344829

--- trax/rl/actor_critic_joint_test.py
+++ trax/rl/actor_critic_joint_test.py
@@ -20,13 +20,16 @@ import functools
 
 from absl.testing import absltest
 
-from trax import layers as tl
+import jax
+
 from trax import lr_schedules
+from trax import math
 from trax import models
 from trax import optimizers as opt
 from trax import test_utils
 from trax.rl import actor_critic_joint
 from trax.rl import task as rl_task
+# from trax.tf_numpy import numpy as tf_np
 
 
 
@@ -36,96 +39,29 @@ class ActorCriticJointTest(absltest.TestCase):
     super().setUp()
     test_utils.ensure_flag('test_tmpdir')
 
-  def test_awrjoint_save_restore(self):
-    """Check save and restore of joint AWR trainer."""
-    task = rl_task.RLTask('CartPole-v0', initial_trajectories=100,
-                          max_steps=200)
-    joint_model = functools.partial(
-        models.PolicyAndValue,
-        body=lambda mode: tl.Serial(tl.Dense(64), tl.Relu()),
-    )
-    tmp_dir = self.create_tempdir().full_path
-    trainer1 = actor_critic_joint.AWRJointTrainer(
-        task,
-        joint_model=joint_model,
-        optimizer=opt.Adam,
-        batch_size=4,
-        train_steps_per_epoch=1,
-        collect_per_epoch=2,
-        output_dir=tmp_dir)
-    trainer1.run(2)
-    self.assertEqual(trainer1.current_epoch, 2)
-    self.assertEqual(trainer1._trainer.step, 2)
-    # Trainer 2 starts where trainer 1 stopped.
-    trainer2 = actor_critic_joint.AWRJointTrainer(
-        task,
-        joint_model=joint_model,
-        optimizer=opt.Adam,
-        batch_size=4,
-        train_steps_per_epoch=1,
-        collect_per_epoch=2,
-        output_dir=tmp_dir)
-    trainer2.run(1)
-    self.assertEqual(trainer2.current_epoch, 3)
-    self.assertEqual(trainer2._trainer.step, 3)
-    trainer1.close()
-    trainer2.close()
+  def test_jointa2ctrainer_atari(self):
+    """Test-runs joint A2C on boxing."""
 
+    # tf_np.set_allow_float64(FLAGS.tf_allow_float64)
 
-  def test_jointppotrainer_cartpole(self):
-    """Test-runs joint PPO on CartPole."""
+    env = environments.load_from_settings(
+        platform='atari',
+        settings={
+            'levelName': 'boxing',
+            'interleaved_pixels': True,
+            'zero_indexed_actions': True
+        })
+    env = atari_wrapper.AtariWrapper(environment=env, num_stacked_frames=1)
 
-    task = rl_task.RLTask('CartPole-v0', initial_trajectories=0,
-                          max_steps=200)
-    joint_model = functools.partial(
-        models.PolicyAndValue,
-        body=lambda mode: tl.Serial(tl.Dense(64), tl.Relu()),
-    )
+    task = rl_task.RLTask(
+        env, initial_trajectories=0, dm_suite=True, max_steps=20)
+
+    body = lambda mode: models.AtariCnnBody()
+    joint_model = functools.partial(models.PolicyAndValue, body=body)
+
     lr = lambda h: lr_schedules.MultifactorSchedule(  # pylint: disable=g-long-lambda
         h, constant=1e-2, warmup_steps=100, factors='constant * linear_warmup')
 
-    trainer = actor_critic_joint.PPOJointTrainer(
-        task,
-        joint_model=joint_model,
-        optimizer=opt.Adam,
-        lr_schedule=lr,
-        batch_size=4,
-        train_steps_per_epoch=2,
-        collect_per_epoch=5)
-    trainer.run(2)
-    self.assertEqual(2, trainer.current_epoch)
-
-  def test_jointawrtrainer_cartpole(self):
-    """Test-runs joint AWR on cartpole."""
-    task = rl_task.RLTask('CartPole-v0', initial_trajectories=100,
-                          max_steps=200)
-    joint_model = functools.partial(
-        models.PolicyAndValue,
-        body=lambda mode: tl.Serial(tl.Dense(64), tl.Relu()),
-    )
-    lr = lambda h: lr_schedules.MultifactorSchedule(  # pylint: disable=g-long-lambda
-        h, constant=1e-2, warmup_steps=100, factors='constant * linear_warmup')
-    trainer = actor_critic_joint.AWRJointTrainer(
-        task,
-        joint_model=joint_model,
-        optimizer=opt.Adam,
-        lr_schedule=lr,
-        batch_size=4,
-        train_steps_per_epoch=2,
-        collect_per_epoch=5)
-    trainer.run(2)
-    self.assertEqual(2, trainer.current_epoch)
-
-  def test_jointa2ctrainer_cartpole(self):
-    """Test-runs joint A2C on cartpole."""
-    task = rl_task.RLTask('CartPole-v0', initial_trajectories=100,
-                          max_steps=200)
-    joint_model = functools.partial(
-        models.PolicyAndValue,
-        body=lambda mode: tl.Serial(tl.Dense(64), tl.Relu()),
-    )
-    lr = lambda h: lr_schedules.MultifactorSchedule(  # pylint: disable=g-long-lambda
-        h, constant=1e-2, warmup_steps=100, factors='constant * linear_warmup')
     trainer = actor_critic_joint.A2CJointTrainer(
         task,
         joint_model=joint_model,
@@ -134,44 +70,12 @@ class ActorCriticJointTest(absltest.TestCase):
         batch_size=2,
         train_steps_per_epoch=1,
         collect_per_epoch=1)
-    trainer.run(2)
-    self.assertEqual(2, trainer.current_epoch)
 
-  def test_jointawrtrainer_cartpole_transformer(self):
-    """Test-runs joint AWR on cartpole with Transformer."""
-    task = rl_task.RLTask('CartPole-v0', initial_trajectories=100,
-                          max_steps=200)
-    body = lambda mode: models.TransformerDecoder(  # pylint: disable=g-long-lambda
-        d_model=32, d_ff=32, n_layers=1, n_heads=1, mode=mode)
-    joint_model = functools.partial(models.PolicyAndValue, body=body)
-    trainer = actor_critic_joint.AWRJointTrainer(
-        task,
-        joint_model=joint_model,
-        optimizer=opt.Adam,
-        batch_size=4,
-        train_steps_per_epoch=2,
-        collect_per_epoch=2,
-        max_slice_length=128)
-    trainer.run(2)
-    self.assertEqual(2, trainer.current_epoch)
-
-  def test_jointa2ctrainer_cartpole_transformer(self):
-    """Test-runs joint A2C on cartpole with Transformer."""
-    task = rl_task.RLTask('CartPole-v0', initial_trajectories=100,
-                          max_steps=200)
-    body = lambda mode: models.TransformerDecoder(  # pylint: disable=g-long-lambda
-        d_model=32, d_ff=32, n_layers=1, n_heads=1, mode=mode)
-    joint_model = functools.partial(models.PolicyAndValue, body=body)
-    trainer = actor_critic_joint.A2CJointTrainer(
-        task,
-        joint_model=joint_model,
-        optimizer=opt.RMSProp,
-        batch_size=4,
-        train_steps_per_epoch=2,
-        collect_per_epoch=2)
-    trainer.run(2)
-    self.assertEqual(2, trainer.current_epoch)
-
+    math.disable_jit()
+    with jax.disable_jit():
+      trainer.run(1)
+      trainer.close()
+    self.assertEqual(1, trainer.current_epoch)
 
 if __name__ == '__main__':
   absltest.main()
