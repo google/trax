Some asserts and a fix in new_log_probs computation.
PiperOrigin-RevId: 309903427

trax/rl/actor_critic_joint.py
@@ -423,6 +423,17 @@ class A2CJointTrainer(ActorCriticJointTrainer):
       """Definition of the A2C loss."""
       del old_log_probs
 
+      # Typically we have dist_inputs of the shape float32[128,1,18]
+      assert len(dist_inputs.shape) == 3, (
+          f'len(dist_inputs.shape) was {len(dist_inputs.shape)}')
+      # values of the shape float32[128,1,1]
+      # returns of the shape float32[128,1,1]
+      assert values.shape == returns.shape, f'values.shape was {values.shape}'
+      # actions of the shape int32[128,1]
+      assert len(actions.shape) == 2, f'actions.shape was {actions.shape}'
+      # and mask of the shape float32[128,1]
+      assert len(mask.shape) == 2, f'mask.shape was {mask.shape}'
+
       a2c_objective = rl_layers.A2CObjective(
           dist_inputs,
           stop_gradient(values),
@@ -430,17 +441,25 @@ class A2CJointTrainer(ActorCriticJointTrainer):
           log_prob_fun=self._policy_dist.log_prob,
           normalize_advantages=self._normalize_advantages)
 
+      # we insist that a2c_objective is a scalar
+      assert jnp.ndim(a2c_objective) == 0, f'a2c_objective was {a2c_objective}'
+
       entropy_loss = rl_layers.EntropyLoss(
           dist_inputs, actions,
           log_prob_fun=self._policy_dist.log_prob,
           entropy_coeff=self._entropy_coeff,
           entropy_fun=self._policy_dist.entropy)
 
-      l2_value_loss = rl_layers.ValueLoss(
-          values, returns,
-          value_loss_coeff=self._value_loss_coeff)
+      assert jnp.ndim(entropy_loss) == 0, f'entropy_loss was {entropy_loss}'
 
-      return a2c_objective.mean() + l2_value_loss - entropy_loss
+      l2_value_loss = rl_layers.ValueLoss(
+          values, returns, value_loss_coeff=self._value_loss_coeff)
+
+      assert jnp.ndim(l2_value_loss) == 0, f'l2_value_loss was {l2_value_loss}'
+
+      combined_loss = a2c_objective + l2_value_loss - entropy_loss
+
+      return combined_loss
 
     return tl.Fn('A2CJointLoss', f, n_out=1)
 
@@ -471,6 +490,7 @@ class A2CJointTrainer(ActorCriticJointTrainer):
   def a2c_objective(self):
     """A2C objective with local parameters."""
     return tl.Fn(
+        'A2CObjective',
         lambda dist_inputs, values, returns, actions, old_log_probs, mask:
         rl_layers.A2CObjective(
             dist_inputs, values, returns, actions, mask,

trax/rl/rl_layers.py
@@ -50,7 +50,9 @@ def NewLogProbs(dist_inputs, actions, log_prob_fun):
   """Given distribution and actions calculate log probs."""
   new_log_probs = log_prob_fun(dist_inputs,
                                actions)
-  new_log_probs = jnp.array(new_log_probs.squeeze(axis=-1))
+  # we want to reformat from e.g. [[-2.8527899], [-2.8768425]]
+  # to [-2.8527899, -2.8768425]
+  # new_log_probs = new_log_probs.flatten()
   return new_log_probs
 
 
@@ -123,11 +125,39 @@ def PPOObjective(dist_inputs, values, returns, actions, old_log_probs,
 def A2CObjective(dist_inputs, values, returns,
                  actions, mask, log_prob_fun, normalize_advantages):
   """Definition of the Advantage Actor Critic (A2C) loss."""
-  returns = returns.squeeze()
-  values = values.squeeze()
+  # values of the shape float32[128,1,1]
+  # returns of the shape float32[128,1,1]
+  # actions of the shape int32[128,1]
+  # and mask of the shape float32[128,1]
+  # We have to squeeze values and returns, because we
+  # are planning to compute (return - values) * new_log_probs * mask
+  # and all of them should be of the same dimension
+  values = values.squeeze(axis=2)
+  returns = returns.squeeze(axis=2)
+
+  assert returns.shape == values.shape, (
+      f'returns.shape was {returns.shape} and values.shape was {values.shape}')
+  assert values.shape == mask.shape, (
+      f'values.shape was {values.shape} and mask.shape was {mask.shape}')
+  assert returns.shape[0] == dist_inputs.shape[0], (
+      f'returns.shape[0] was {returns.shape[0]} and dist_inputs.shape[0] was '
+      f'{dist_inputs.shape[0]}')
+
   new_log_probs = NewLogProbs(dist_inputs, actions, log_prob_fun)
+  assert new_log_probs.shape == mask.shape, (
+      f'new_log_probs.shape was {new_log_probs.shape} and mask.shape was '
+      f'{mask.shape}')
+
   advantages = returns - values
   if normalize_advantages:
     advantages = advantages - jnp.mean(advantages)
     advantages /= jnp.std(advantages) + 1e-8
-  return -jnp.sum(new_log_probs * advantages * mask) / jnp.sum(mask)
+  assert new_log_probs.shape == advantages.shape, (
+      f'new_log_probs.shape was {new_log_probs.shape} and advantages.shape was '
+      f'{advantages.shape}')
+
+  # One of the motivation to the squeezes and assertions is to
+  # avoid [128,1] * [128,1,1] * [128] multiplications in the definition
+  # of the a2c objective - we insist on the same shapes
+  a2c_objective = -jnp.sum(new_log_probs * advantages * mask) / jnp.sum(mask)
+  return a2c_objective