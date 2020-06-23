Correct ReLU for a good gradient at 0, remove obsolete pretrain_finetune.
PiperOrigin-RevId: 317819381

trax/layers/activation_fns.py
@@ -38,7 +38,7 @@ def Relu():
           x & \text{otherwise}.
       \end{array} \right.
   """
-  return Fn('Relu', lambda x: np.maximum(x, np.zeros_like(x)))
+  return Fn('Relu', lambda x: np.where(x <= 0, np.zeros_like(x), x))
 
 
 def ParametricRelu(a=1.):