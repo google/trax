3b76f0e74782397475ce8afe1103bbec7d93f761
Peng Wang <wangpeng@google.com>
2020-07-06 18:43:47 -0700

    [TF-numpy] Re-enables signature check against numpy, and fixes problems found by the check.
    PiperOrigin-RevId: 319898429

--- trax/trax2keras_test.py
+++ trax/trax2keras_test.py
@@ -171,7 +171,7 @@ class Trax2KerasTest(tf.test.TestCase, parameterized.TestCase):
             keras_layer.trainable_variables, keras_grads)
         self.assertAllClose(
             to_tensors(weights), read_values(keras_layer._weights),
-            rtol=2e-6, atol=5e-5)
+            rtol=2e-6, atol=2e-4)
         self.assertAllClose(to_tensors(state), read_values(keras_layer._state))
         self.assertAllClose(to_tensors(rng), read_values(keras_layer._rng))
       if use_model:
