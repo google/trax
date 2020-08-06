85fd4519e2e6e04db1ff0f56a5917220512b9fad
facudeza <facudeza@debian.facudeza>
2020-08-06 16:29:32 +0200

    positional encoding modification

--- trax/models/reformer/reformer.py
+++ trax/models/reformer/reformer.py
@@ -647,6 +647,7 @@ def Reformer(input_vocab_size,
     #positional_encoding = tl.PositionalEncoding(
         #max_len=max_len, dropout=dropout, mode=mode)
     return [
+ 	tl.Select([3]),
         tl.Dense( d_model),
         tl.Dropout(rate=dropout, shared_axes=None, mode=mode),
         tl.PositionalEncoding(max_len=max_len)
@@ -699,7 +700,7 @@ def Reformer(input_vocab_size,
   return tl.Serial(
       # Input: encoder_side_tokens, decoder_side_tokens
       # Copy decoder tokens for use in loss.
-      tl.Select([0, 0, 0 ,0]),                 # tok_e tok_d tok_d vec_e
+      tl.Select([0, 1, 1 ,2]),                 # tok_e tok_d tok_d vec_e
       tl.Branch([], [tl.PaddingMask(),
                      tl.Fn('Squeeze',
                            lambda x: jnp.squeeze(x, (1, 2)), n_out=1)]),
