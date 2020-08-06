74da5c3e30fab3ca73eac8a2b6e9e38b37b453ee
facudeza <facudeza@debian.facudeza>
2020-08-06 16:24:20 +0200

    positional encoding modification

--- trax/models/reformer/reformer.py
+++ trax/models/reformer/reformer.py
@@ -707,7 +707,7 @@ def Reformer(input_vocab_size,
 
       # Encode.
 
-      tl.Select([2, 1, 2]),                 # vec_e  mask tok_d
+      tl.Select([3, 3, 3, 3]),                 # vec_e  mask tok_d
 
       encoder,                              # vec_e  mask tok_d .....
 
