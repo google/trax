455093506f277f5f1a4af8a6f191b5c2245f1a80
facudeza <facudeza@debian.facudeza>
2020-08-08 18:43:07 +0200

    data stack

--- trax/models/reformer/reformer.py
+++ trax/models/reformer/reformer.py
@@ -691,7 +691,7 @@ def Reformer(input_vocab_size=None,
   return tl.Serial(
       # Input: encoder_side_tokens, decoder_side_tokens
       # Copy decoder tokens for use in loss.
-      tl.Select([0, 1, 1 ,2]),                 # tok_e tok_d tok_d vec_e
+      tl.Select([0, 2, 2 ,1]),                 # tok_e tok_d tok_d vec_e
       tl.Branch([], [tl.PaddingMask(),
                      tl.Fn('Squeeze',
                            lambda x: jnp.squeeze(x , (1,2)), n_out=1)]),
