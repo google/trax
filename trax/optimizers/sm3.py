949183847e67393d8c57e55029bee968b625e9ec
Rohan Anil <rohananil@google.com>
2021-03-05 15:37:00 -0800

    Add grafting to SM3, when the beta2 < 1.0, so there is natural layerwise decay.
    PiperOrigin-RevId: 361239185

--- trax/optimizers/sm3.py
+++ trax/optimizers/sm3.py
@@ -53,6 +53,7 @@ class SM3(opt_base.Optimizer):
     """
     self._has_momentum = momentum > 0.0
     self._momentum_type = momentum_type
+    self._graft = second_moment_averaging != 1.0
     super().__init__(
         learning_rate=learning_rate,
         momentum=momentum,
@@ -60,16 +61,18 @@ class SM3(opt_base.Optimizer):
         weight_decay=weight_decay,
     )
 
-  def init(self, weights):
+  def init(self, w):
     momentum = []
     if self._has_momentum:
-      momentum = jnp.zeros_like(weights)
-    vs = [jnp.zeros(sz, dtype=weights.dtype) for sz in weights.shape]
-    return (momentum, vs)
+      momentum = jnp.zeros_like(w)
+    v1s = [jnp.zeros(sz, dtype=w.dtype) for sz in w.shape]
+    v2s = []
+    if self._graft:
+      v2s = [jnp.zeros(sz, dtype=w.dtype) for sz in w.shape]
+    return (momentum, v1s, v2s)
 
   def _momentum_update(self, g, m, beta1):
     """Handle various types of momentum."""
-    print(self._momentum_type)
     if self._momentum_type == MomentumType.EMA:
       m = (1 - beta1) * g + beta1 * m
       update = m
@@ -84,18 +87,28 @@ class SM3(opt_base.Optimizer):
       assert False, 'Unknown momentum_type.'
     return m, update
 
-  def _update_diagonal(self, g, w, m, v, opt_params):
+  def _update_diagonal(self, g, w, m, v1, v2, opt_params):
     learning_rate = opt_params['learning_rate']
     beta2 = opt_params['second_moment_averaging']
     weight_decay = opt_params['weight_decay']
 
     is_beta2_1 = (beta2 == 1).astype(g.dtype)
     w = is_beta2_1  + (1.0 - beta2) * (1.0 - is_beta2_1)
-    v[0] = beta2 * v[0] + w * g * g
+    v1[0] = beta2 * v1[0] + w * g * g
 
-    preconditioner = jnp.where(v[0] > 0, 1.0 / (jnp.sqrt(v[0]) + 1e-16),
-                               jnp.zeros_like(v[0]))
+    preconditioner = jnp.where(v1[0] > 0, 1.0 / (jnp.sqrt(v1[0]) + 1e-16),
+                               jnp.zeros_like(v1[0]))
+
     pg = preconditioner * g
+    if self._graft:
+      v2[0] += g * g
+      preconditioner_graft = jnp.where(
+          v2[0] > 0, 1.0 / (jnp.sqrt(v2[0]) + 1e-16), jnp.zeros_like(v2[0]))
+      pg_graft = preconditioner_graft * g
+      pg_norm = jnp.linalg.norm(pg)
+      pg_graft_norm = jnp.linalg.norm(pg_graft)
+      pg = pg * (pg_graft_norm/(pg_norm + 1e-16))
+
     pg = pg + w * weight_decay
 
     if self._has_momentum:
@@ -104,7 +117,7 @@ class SM3(opt_base.Optimizer):
       update = pg
 
     w = w - (update * learning_rate).astype(w.dtype)
-    return w, (m, v)
+    return w, (m, v1, v2)
 
   def _expanded_shape(self, shape, axis):
     # Replaces a `shape` of [M, N, K] with 1 in all dimensions except for i.
@@ -118,7 +131,7 @@ class SM3(opt_base.Optimizer):
       minimum = jnp.minimum(minimum, tensor_list[i])
     return minimum
 
-  def _update_sketched(self, g, w, m, v, opt_params):
+  def _update_sketched(self, g, w, m, v1, v2, opt_params):
     """Update for higher-rank parameters."""
     learning_rate = opt_params['learning_rate']
     momentum = opt_params['momentum']
@@ -127,7 +140,7 @@ class SM3(opt_base.Optimizer):
 
     shape = w.shape
     rank = len(shape)
-    reshaped_accumulators = [jnp.reshape(v[i], self._expanded_shape(shape, i))
+    reshaped_accumulators = [jnp.reshape(v1[i], self._expanded_shape(shape, i))
                              for i in range(rank)]
     acc = self._minimum(reshaped_accumulators)
 
@@ -138,6 +151,20 @@ class SM3(opt_base.Optimizer):
     preconditioner = jnp.where(acc > 0.0, 1.0 / (jnp.sqrt(acc) + 1e-16),
                                jnp.zeros_like(acc))
     pg = g * preconditioner
+    if self._graft:
+      v2_acc = self._minimum([
+          jnp.reshape(v2[i], self._expanded_shape(shape, i))
+          for i in range(rank)
+      ])
+      v2_acc = v2_acc + g * g
+      preconditioner_graft = jnp.where(v2_acc > 0.0,
+                                       1.0 / (jnp.sqrt(v2_acc) + 1e-16),
+                                       jnp.zeros_like(v2_acc))
+      pg_graft = preconditioner_graft * g
+      pg_norm = jnp.linalg.norm(pg)
+      pg_graft_norm = jnp.linalg.norm(pg_graft)
+      pg = pg * (pg_graft_norm/(pg_norm + 1e-16))
+
     pg = pg + w * weight_decay
 
     if self._has_momentum:
@@ -146,17 +173,23 @@ class SM3(opt_base.Optimizer):
       update = pg
 
     w = w - (learning_rate * update).astype(w.dtype)
-    for i in range(len(v)):
+    for i in range(len(v1)):
       axes = list(range(int(i))) + list(range(int(i) + 1, rank))
       dim_accumulator = jnp.amax(acc, axis=axes)
-      v[i] = dim_accumulator
-    return w, (m, v)
+      v1[i] = dim_accumulator
+
+    if self._graft:
+      for i in range(len(v2)):
+        axes = list(range(int(i))) + list(range(int(i) + 1, rank))
+        dim_accumulator = jnp.amax(v2_acc, axis=axes)
+        v2[i] = dim_accumulator
+    return w, (m, v1, v2)
 
   def update(self, step, g, w, slots, opt_params):
     del step
-    m, v = slots
+    m, v1, v2 = slots
     rank = len(w.shape)
     if rank > 1:
-      return self._update_sketched(g, w, m, v, opt_params)
+      return self._update_sketched(g, w, m, v1, v2, opt_params)
     else:
-      return self._update_diagonal(g, w, m, v, opt_params)
+      return self._update_diagonal(g, w, m, v1, v2, opt_params)
