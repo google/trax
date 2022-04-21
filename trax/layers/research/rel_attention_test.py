a0ac26dfef4fab14f604474176127e0c337a1568
Yilei Yang <yileiyang@google.com>
2022-04-21 13:16:57 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 443460698

--- trax/layers/research/rel_attention_test.py
+++ trax/layers/research/rel_attention_test.py
@@ -27,7 +27,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Tests for trax.layers.relattention."""
 
 from absl.testing import absltest
