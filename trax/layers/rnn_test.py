241ce7e84a28c4124f1903c1f991794f0ccb52ca
mmarcinmichal <m.marcinmichal@gmail.com>
2023-10-02 12:54:24 +0200

    + build

--- trax/layers/rnn_test.py
+++ trax/layers/rnn_test.py
@@ -27,51 +27,49 @@ import trax.layers as tl
 BACKENDS = [fastmath.Backend.JAX]
 
 
-@parameterized.named_parameters(
-    ('_' + b.value, b) for b in BACKENDS)
+@parameterized.named_parameters(("_" + b.value, b) for b in BACKENDS)
 class RnnTest(parameterized.TestCase):
+    def test_conv_gru_cell(self, backend):
+        with fastmath.use_backend(backend):
+            layer = tl.ConvGRUCell(9, kernel_size=(3, 3))
+            x = np.ones((8, 1, 7, 9))
+            _, _ = layer.init(shapes.signature(x))
+            y = layer(x)
+            self.assertEqual(y.shape, x.shape)
 
-  def test_conv_gru_cell(self, backend):
-    with fastmath.use_backend(backend):
-      layer = tl.ConvGRUCell(9, kernel_size=(3, 3))
-      x = np.ones((8, 1, 7, 9))
-      _, _ = layer.init(shapes.signature(x))
-      y = layer(x)
-      self.assertEqual(y.shape, x.shape)
+    def test_gru_cell(self, backend):
+        with fastmath.use_backend(backend):
+            layer = tl.GRUCell(9)
+            xs = [np.ones((8, 7, 9)), np.ones((8, 7, 9))]
+            _, _ = layer.init(shapes.signature(xs))
+            ys = layer(xs)
+            self.assertEqual([y.shape for y in ys], [(8, 7, 9), (8, 7, 9)])
 
-  def test_gru_cell(self, backend):
-    with fastmath.use_backend(backend):
-      layer = tl.GRUCell(9)
-      xs = [np.ones((8, 7, 9)), np.ones((8, 7, 9))]
-      _, _ = layer.init(shapes.signature(xs))
-      ys = layer(xs)
-      self.assertEqual([y.shape for y in ys], [(8, 7, 9), (8, 7, 9)])
+    def test_lstm_cell(self, backend):
+        with fastmath.use_backend(backend):
+            layer = tl.LSTMCell(9)
+            xs = [np.ones((8, 9)), np.ones((8, 18))]
+            _, _ = layer.init(shapes.signature(xs))
+            ys = layer(xs)
+            self.assertEqual([y.shape for y in ys], [(8, 9), (8, 18)])
 
-  def test_lstm_cell(self, backend):
-    with fastmath.use_backend(backend):
-      layer = tl.LSTMCell(9)
-      xs = [np.ones((8, 9)), np.ones((8, 18))]
-      _, _ = layer.init(shapes.signature(xs))
-      ys = layer(xs)
-      self.assertEqual([y.shape for y in ys], [(8, 9), (8, 18)])
+    def test_sru(self, backend):
+        with fastmath.use_backend(backend):
+            layer = tl.SRU(7)
+            x = [np.ones((8, 9, 7), np.float32)]
+            _, _ = layer.init(shapes.signature(x))
+            y = layer(x)
+            self.assertEqual(y.shape, x.shape)
 
-  def test_sru(self, backend):
-    with fastmath.use_backend(backend):
-      layer = tl.SRU(7)
-      x = np.ones((8, 9, 7), np.float32)
-      _, _ = layer.init(shapes.signature(x))
-      y = layer(x)
-      self.assertEqual(y.shape, x.shape)
-
-  def test_names(self, backend):
-    with fastmath.use_backend(backend):
-      layer = tl.LSTM(3)
-      self.assertEqual('LSTM_3', str(layer))
-      layer = tl.GRU(5)
-      self.assertEqual('GRU_5', str(layer))
-      layer = tl.SRU(7)
-      self.assertEqual('SRU_7', str(layer))
+    def test_names(self, backend):
+        with fastmath.use_backend(backend):
+            layer = tl.LSTM(3)
+            self.assertEqual("LSTM_3", str(layer))
+            layer = tl.GRU(5)
+            self.assertEqual("GRU_5", str(layer))
+            layer = tl.SRU(7)
+            self.assertEqual("SRU_7", str(layer))
 
 
-if __name__ == '__main__':
-  absltest.main()
+if __name__ == "__main__":
+    absltest.main()
